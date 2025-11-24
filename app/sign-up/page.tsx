056cbca5757ab5bb0b608f6071537dec91ec9c04
Sarangerel624 <sarangerelbatbold11@gmail.com>
2025-11-24 13:45:01 +0800

    save signup changes

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,18 +1,15 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { useState, useEffect } from "react"
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
 
 const Page = () => {
-
-    return (
-        <div>
-            <div>
-                hii
-            </div>
-            <Button className="">submit</Button>     
-               </div>
-    )
-}
+  return (
+    <div>
+      <div>hii</div>
+      <Button className="">submit</Button>
+    </div>
+  );
+};
 
 export default Page;
